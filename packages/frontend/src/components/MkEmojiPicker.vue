e21d7c5f1f7bce08acd40f10fd815881b8e0ce2c
FruitRiin <nassii74@gmail.com>
2024-11-22 14:22:19 +0900

    overwrite mobile emoji colmuns size

--- packages/frontend/src/components/MkEmojiPicker.vue
+++ packages/frontend/src/components/MkEmojiPicker.vue
@@ -604,8 +604,8 @@ defineExpose({
 
 					> .item {
 						aspect-ratio: 1 / 1;
-						width: auto;
-						height: auto;
+						// width: auto;
+						// height: auto;
 						min-width: 0;
 
 						&:disabled {
