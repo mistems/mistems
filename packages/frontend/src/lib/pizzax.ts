3625e9d4638a574d34510aeb6ceee4926cdcce9c
fruitriin <nassii74@gmail.com>
2026-02-01 01:09:01 +0900

    fix(frontend): すべてのアカウントからログアウトされる問題を修正

--- packages/frontend/src/lib/pizzax.ts
+++ packages/frontend/src/lib/pizzax.ts
@@ -12,7 +12,7 @@ import { BroadcastChannel } from 'broadcast-channel';
 import type { Ref } from 'vue';
 import { $i } from '@/i.js';
 import { misskeyApi } from '@/utility/misskey-api.js';
-import { get, set } from '@/utility/idb-proxy.js';
+import { get, set, delMany } from '@/utility/idb-proxy.js';
 import { store } from '@/store.js';
 import { deepClone } from '@/utility/clone.js';
 import { deepMerge } from '@/utility/merge.js';
@@ -220,6 +220,18 @@ export class Pizzax<T extends StateDef> {
 	public reset(key: keyof T) {
 		this.set(key, this.def[key].default);
 		return this.def[key].default;
+	}
+
+	/** 現在のアカウントに紐づくデータをデバイスから削除します */
+	public async clearAccountDataFromDevice(id = $i?.id) {
+		if (id == null) return;
+
+		const deviceAccountStateKey = `pizzax::${this.key}::${id}` satisfies typeof this.deviceAccountStateKeyName;
+		const registryCacheKey = `pizzax::${this.key}::cache::${id}` satisfies typeof this.registryCacheKeyName;
+
+		await this.addIdbSetJob(async () => {
+			await delMany([deviceAccountStateKey, registryCacheKey]);
+		});
 	}
 
 	/**
