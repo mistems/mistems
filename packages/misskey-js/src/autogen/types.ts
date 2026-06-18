e2fa90cef9c281b6dbe445344d927da9cdb9502d
fruitriin <nassii74@gmail.com>
2026-06-18 22:14:45 +0900

    mistems-main.build-misskey-js-with-types 再生成

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -20873,7 +20873,7 @@ export interface operations {
                 };
                 content: {
                     'application/json': {
-                        emojis: components['schemas']['EmojiSimple'][];
+                        emojis: components['schemas']['EmojiDetailed'][];
                     };
                 };
             };
