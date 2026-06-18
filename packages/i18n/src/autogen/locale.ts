c5b8bdafcfc66bb601812b46924505d82bfa76d3
fruitriin <nassii74@gmail.com>
2026-06-18 22:14:23 +0900

    fix(frontend): すべてのアカウントからログアウトされる問題を修正

--- packages/i18n/src/autogen/locale.ts
+++ packages/i18n/src/autogen/locale.ts
@@ -153,6 +153,14 @@ export interface Locale extends ILocale {
      */
     "logout": string;
     /**
+     * このアカウントからログアウト
+     */
+    "logoutFromThisAccount": string;
+    /**
+     * すべてのアカウントからログアウト
+     */
+    "logoutFromAll": string;
+    /**
      * 新規登録
      */
     "signup": string;
@@ -3992,6 +4000,10 @@ export interface Locale extends ILocale {
      * ログアウトするとクライアントの設定情報がブラウザから消去されます。再ログイン時に設定情報を復元できるようにするためには、設定の自動バックアップを有効にしてください。
      */
     "logoutWillClearClientData": string;
+    /**
+     * {username}からログアウトしますか？
+     */
+    "logoutFromOtherAccountConfirm": ParameterizedString<"username">;
     /**
      * 最終利用日時
      */
