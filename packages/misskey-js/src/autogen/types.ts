f0acccb00186a3d155be8814723adbabfd51cf31
fruitriin <nassii74@gmail.com>
2026-07-05 14:30:35 +0900

    MkReactionViewer拡張（よみがなさっと見る）

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -20947,7 +20947,7 @@ export interface operations {
                 };
                 content: {
                     'application/json': {
-                        emojis: components['schemas']['EmojiSimple'][];
+                        emojis: components['schemas']['EmojiDetailed'][];
                     };
                 };
             };
