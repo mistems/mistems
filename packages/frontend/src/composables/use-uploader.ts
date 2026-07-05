623b2d71bd1556e9d903a728e13908ce8d56ec48
fruitriin <nassii74@gmail.com>
2026-07-05 09:41:53 +0900

    MkPagesエディター拡張/ドラッグアンドドロップのスマホ拡張とアニメーション

--- packages/frontend/src/composables/use-uploader.ts
+++ packages/frontend/src/composables/use-uploader.ts
@@ -776,10 +776,15 @@ export function useUploader(options: {
 		dispose();
 	});
 
+	function reorderItems(newItems: UploaderItem[]) {
+		items.value = newItems;
+	}
+
 	return {
 		items,
 		addFiles,
 		removeItem,
+		reorderItems,
 		abortAll,
 		reset,
 		dispose,
