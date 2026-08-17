182ef7e5f7404b275f2907ba1efd47e0f8de3a14
fruitriin <nassii74@gmail.com>
2026-08-17 14:02:52 +0900

    ノート検索の強化

--- packages/i18n/src/autogen/locale.ts
+++ packages/i18n/src/autogen/locale.ts
@@ -12622,6 +12622,10 @@ export interface Locale extends ILocale {
          */
         "searchScopeUser": string;
         /**
+         * チャンネル指定
+         */
+        "searchScopeChannel": string;
+        /**
          * サーバーのホストを入力してください
          */
         "pleaseEnterServerHost": string;
@@ -12630,17 +12634,29 @@ export interface Locale extends ILocale {
          */
         "pleaseSelectUser": string;
         /**
+         * チャンネルを選択してください
+         */
+        "pleaseSelectChannel": string;
+        /**
+         * お気に入りに登録したチャンネルがありません
+         */
+        "noFavoritedChannels": string;
+        /**
          * 例: misskey.example.com
          */
         "serverHostPlaceholder": string;
         /**
-         * 投稿日時from
+         * この日から
          */
         "postFrom": string;
         /**
-         * 投稿日時to
+         * この日まで
          */
         "postTo": string;
+        /**
+         * スペース区切りで絞り込み (AND)、「A OR B」でいずれか、「-単語」で除外、「"語句"」でフレーズ検索
+         */
+        "querySyntaxHelp": string;
     };
     "_serverSetupWizard": {
         /**
