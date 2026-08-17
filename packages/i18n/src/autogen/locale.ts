4c70630d5d77c1d8a6ea9589982db5c15d6100fc
fruitriin <nassii74@gmail.com>
2026-08-17 14:06:11 +0900

    MkPagesエディター拡張/ドラッグアンドドロップのスマホ拡張とアニメーション

--- packages/i18n/src/autogen/locale.ts
+++ packages/i18n/src/autogen/locale.ts
@@ -10820,6 +10820,10 @@ export interface Locale extends ILocale {
          * 特殊
          */
         "specialBlocks": string;
+        /**
+         * 他MFM
+         */
+        "otherMfm": string;
         "blocks": {
             /**
              * テキスト
