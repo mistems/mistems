e28b107e62a6a86d93c059d4bd44e299147a7745
fruitriin <nassii74@gmail.com>
2026-09-24 12:14:58 +0900

    MkNote拡張（チャンネルRenoteどこからきてどこへいくのか）

--- packages/i18n/src/autogen/locale.ts
+++ packages/i18n/src/autogen/locale.ts
@@ -93,6 +93,10 @@ export interface Locale extends ILocale {
      */
     "renotedBy": ParameterizedString<"user">;
     /**
+     * {user}が{toChannel}へリノート
+     */
+    "renotedByToChannel": ParameterizedString<"user" | "toChannel">;
+    /**
      * ノートはありません
      */
     "noNotes": string;
