fddaa3ecd58933d638bc9083db376f0d63607f6a
fruitriin <nassii74@gmail.com>
2026-09-24 12:16:39 +0900

    通知の残カウントバグを直す

--- packages/misskey-js/src/autogen/types.ts
+++ packages/misskey-js/src/autogen/types.ts
@@ -4162,6 +4162,7 @@ export type components = {
             hasUnreadChatMessages: boolean;
             hasUnreadNotification: boolean;
             hasPendingReceivedFollowRequest: boolean;
+            /** @description Saturates at 100. The actual number of unread notifications may be larger. */
             unreadNotificationsCount: number;
             mutedWords: string[][];
             hardMutedWords: string[][];
