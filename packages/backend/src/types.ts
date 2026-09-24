ea4bba2db4698677ce7dd995fc1af8e30db71bff
fruitriin <nassii74@gmail.com>
2026-09-24 12:21:05 +0900

    お知らせの既読をリセットできるように (misskey-dev#15049)

--- packages/backend/src/types.ts
+++ packages/backend/src/types.ts
@@ -104,6 +104,8 @@ export const moderationLogTypes = [
 	'updateUserAnnouncement',
 	'deleteGlobalAnnouncement',
 	'deleteUserAnnouncement',
+	'resetReadsForGlobalAnnouncement',
+	'resetReadsForUserAnnouncement',
 	'resetPassword',
 	'suspendRemoteInstance',
 	'unsuspendRemoteInstance',
@@ -246,6 +248,17 @@ export type ModerationLogPayloads = {
 		announcement: any;
 	};
 	deleteUserAnnouncement: {
+		announcementId: string;
+		announcement: any;
+		userId: string;
+		userUsername: string;
+		userHost: string | null;
+	};
+	resetReadsForGlobalAnnouncement: {
+		announcementId: string;
+		announcement: any;
+	};
+	resetReadsForUserAnnouncement: {
 		announcementId: string;
 		announcement: any;
 		userId: string;
