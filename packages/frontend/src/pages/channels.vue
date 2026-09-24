3455a4e686e662382e6bb14008e3fe7af7abe622
fruitriin <nassii74@gmail.com>
2026-09-24 12:14:57 +0900

    チャンネルだいたいぜんぶみる

--- packages/frontend/src/pages/channels.vue
+++ packages/frontend/src/pages/channels.vue
@@ -6,29 +6,13 @@ SPDX-License-Identifier: AGPL-3.0-only
 <template>
 <PageWithHeader v-model:tab="tab" :actions="headerActions" :tabs="headerTabs" :swipable="true">
 	<div class="_spacer" style="--MI_SPACER-w: 1200px;">
-		<div v-if="tab === 'search'" :class="$style.searchRoot">
-			<div class="_gaps">
-				<MkInput v-model="searchQuery" :large="true" :autofocus="true" type="search" @enter="search">
-					<template #prefix><i class="ti ti-search"></i></template>
-				</MkInput>
-				<MkRadios
-					v-model="searchType"
-					:options="[
-						{ value: 'nameAndDescription', label: i18n.ts._channel.nameAndDescription },
-						{ value: 'nameOnly', label: i18n.ts._channel.nameOnly },
-					]"
-					@update:modelValue="search()"
-				>
-				</MkRadios>
-				<MkButton large primary gradate rounded @click="search">{{ i18n.ts.search }}</MkButton>
-			</div>
-
-			<MkFoldableSection v-if="channelPaginator">
-				<template #header>{{ i18n.ts.searchResult }}</template>
-				<MkChannelList :key="key" :paginator="channelPaginator"/>
-			</MkFoldableSection>
+		<div v-if="tab === 'index'">
+			<MkChannelIndex />
 		</div>
-		<div v-if="tab === 'featured'">
+		<div v-if="tab === 'search'" >
+			<MkChannelSearch />
+		</div>
+		<div v-if="tab === 'featured'" key="featured">
 			<MkPagination v-slot="{items}" :paginator="featuredPaginator">
 				<div :class="$style.root">
 					<MkChannelPreview v-for="channel in items" :key="channel.id" :channel="channel"/>
@@ -62,39 +46,18 @@ SPDX-License-Identifier: AGPL-3.0-only
 </template>
 
 <script lang="ts" setup>
-import { computed, markRaw, onMounted, ref, shallowRef } from 'vue';
+import { computed, markRaw, ref } from 'vue';
+import MkChannelIndex from '@/components/MkChannelIndex.vue';
 import MkChannelPreview from '@/components/MkChannelPreview.vue';
-import MkChannelList from '@/components/MkChannelList.vue';
+import MkChannelSearch from "@/components/MkChannelSearch.vue";
 import MkPagination from '@/components/MkPagination.vue';
-import MkInput from '@/components/MkInput.vue';
-import MkRadios from '@/components/MkRadios.vue';
 import MkButton from '@/components/MkButton.vue';
-import MkFoldableSection from '@/components/MkFoldableSection.vue';
 import { definePage } from '@/page.js';
 import { i18n } from '@/i18n.js';
-import { useRouter } from '@/router.js';
 import { Paginator } from '@/utility/paginator.js';
 import { $i } from '@/i.js';
 
-const router = useRouter();
-
-type SearchType = 'nameAndDescription' | 'nameOnly';
-
-const props = defineProps<{
-	query: string;
-	type?: SearchType;
-}>();
-
-const key = ref('');
 const tab = ref('featured');
-const searchQuery = ref('');
-const searchType = ref<SearchType>('nameAndDescription');
-const channelPaginator = shallowRef();
-
-onMounted(() => {
-	searchQuery.value = props.query ?? '';
-	searchType.value = props.type ?? 'nameAndDescription';
-});
 
 const featuredPaginator = markRaw(new Paginator('channels/featured', {
 	limit: 10,
@@ -111,52 +74,35 @@ const ownedPaginator = markRaw(new Paginator('channels/owned', {
 	limit: 10,
 }));
 
-async function search() {
-	const query = searchQuery.value.toString().trim();
-
-	if (query == null) return;
-
-	const type = searchType.value.toString().trim();
-
-	if (type !== 'nameAndDescription' && type !== 'nameOnly') {
-		console.error(`Unrecognized search type: ${type}`);
-		return;
-	}
-
-	channelPaginator.value = markRaw(new Paginator('channels/search', {
-		limit: 10,
-		params: {
-			query: searchQuery.value,
-			type: type,
-		},
-	}));
-
-	key.value = query + type;
-}
-
 const headerActions = computed(() => []);
 
-const headerTabs = computed(() => [{
-	key: 'search',
-	title: i18n.ts.search,
-	icon: 'ti ti-search',
-}, {
-	key: 'featured',
-	title: i18n.ts._channel.featured,
-	icon: 'ti ti-comet',
-}, {
-	key: 'favorites',
-	title: i18n.ts.favorites,
-	icon: 'ti ti-star',
-}, {
-	key: 'following',
-	title: i18n.ts._channel.following,
-	icon: 'ti ti-eye',
-}, {
-	key: 'owned',
-	title: i18n.ts._channel.owned,
-	icon: 'ti ti-edit',
-}]);
+const headerTabs = computed(() => [
+	{
+		key: 'index',
+		title: 'だいたいぜんぶ',
+		icon: 'ti ti-plus',
+	},
+	{
+		key: 'search',
+		title: i18n.ts.search,
+		icon: 'ti ti-search',
+	}, {
+		key: 'featured',
+		title: i18n.ts._channel.featured,
+		icon: 'ti ti-comet',
+	}, {
+		key: 'favorites',
+		title: i18n.ts.favorites,
+		icon: 'ti ti-star',
+	}, {
+		key: 'following',
+		title: i18n.ts._channel.following,
+		icon: 'ti ti-eye',
+	}, {
+		key: 'owned',
+		title: i18n.ts._channel.owned,
+		icon: 'ti ti-edit',
+	}]);
 
 definePage(() => ({
 	title: i18n.ts.channel,
@@ -165,12 +111,6 @@ definePage(() => ({
 </script>
 
 <style lang="scss" module>
-.searchRoot {
-	width: 100%;
-	max-width: 700px;
-	margin: 0 auto;
-}
-
 .root {
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(360px, 1fr));
